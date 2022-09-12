a473c26fa9f232732ba088685b144d3cdbc914ae
kirti singh <kirtisingh.3007@gmail.com>
2022-09-12 15:49:01 +0200

    renamed project folders

--- day17/quention_model.py
+++
@@ -1,8 +0,0 @@
-
-
-
-class Question:
-    def __init__(self,text,answer):
-        self.txt = text
-        self.ans = answer
-    
--- day19/turtel_playground.py
+++
@@ -1,9 +0,0 @@
-from turtle import Turtle,Screen
-jim = Turtle()
-
-jim.color("blue")
-jim.stamp()
-jim.fd(50)
-jim.clearstamp(-2)
-screen = Screen()
-screen.exitonclick()
