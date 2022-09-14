9f876c31e3661e1b010d28b57b6d940a6263ee8c
kirti singh <kirtisingh.3007@gmail.com>
2022-09-14 10:56:36 +0200

    changed path

--- Mail Merge Project Start(day24)/main.py
+++ Mail Merge Project Start(day24)/main.py
@@ -1,34 +1,34 @@
 ##TODO: Create a letter using starting_letter.txt 
 
-with open("day24_Mail Merge Project Start/Input/Letters/starting_letter.txt") as file:
-    letter_file = file.readlines()
+# with open("day24_Mail Merge Project Start/Input/Letters/starting_letter.txt") as file:
+#     letter_file = file.readlines()
     
-##for each name in invited_names.txt
+# ##for each name in invited_names.txt
 
-with open("day24_Mail Merge Project Start/Input/Names/invited_names.txt") as f:
-    name_file = f.readlines()
+# with open("day24_Mail Merge Project Start/Input/Names/invited_names.txt") as f:
+#     name_file = f.readlines()
 
-for nam in name_file:
-    nam_letter = letter_file.copy()
-    nam_letter[0] = nam_letter[0].replace("[name]",nam.strip())
-    with open(f'day24_Mail Merge Project Start/readytosend_{nam}_.txt', 'w') as output_file:
-        for letter in nam_letter:
-            output_file.write(letter)
+# for nam in name_file:
+#     nam_letter = letter_file.copy()
+#     nam_letter[0] = nam_letter[0].replace("[name]",nam.strip())
+#     with open(f'day24_Mail Merge Project Start/readytosend_{nam}_.txt', 'w') as output_file:
+#         for letter in nam_letter:
+#             output_file.write(letter)
 
 """ alternative solution """
 
 PLACEHOLDER = "[name]"
 
-with open("day24_Mail Merge Project Start/Input/Letters/starting_letter.txt") as letter_file:
+with open("Mail Merge Project Start(day24)/Input/Letters/starting_letter.txt") as letter_file:
     letter_content = letter_file.read()
     #print(letter_content)
 
-with open("day24_Mail Merge Project Start/Input/Names/invited_names.txt") as names_file:
+with open("Mail Merge Project Start(day24)/Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
     for name in names:
         strip_name = name.strip()
         new_letter = letter_content.replace(PLACEHOLDER,strip_name)
-        with open(f"day24_Mail Merge Project Start/Output/ReadyToSend {strip_name}.txt","w") as complete_letter:
+        with open(f"Mail Merge Project Start(day24)/Output/ReadyToSend {strip_name}.txt","w") as complete_letter:
             complete_letter.write(new_letter)
             
 
